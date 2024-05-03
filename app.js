87777fd0f3458a293899da3f5dc2f16e0ae9be67
adamwisnup <wisnuadam21@gmail.com>
2024-05-03 14:15:21 +0700

    Refactor app.js to use app.listen instead of module.exports

--- app.js
+++ app.js
@@ -15,8 +15,4 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1", indexRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
-
-// module.exports = app;
+module.exports = app;
